080488fc49420b30f57dbecfb1337b7593df1e8b
景 <>
2020-03-24 13:50:32 +0800

    fix(templates/mobile/babel.config.js): 新的zv-ui(即vant-ui)从less转换成了scss

--- templates/mobile/babel.config.js
+++ templates/mobile/babel.config.js
@@ -6,7 +6,7 @@ module.exports = {
         {
           libraryName: '@zvalley/zv-ui',
           libraryDirectory: 'es',
-          style: name => `${name}/style/less`
+          style: name => `${name}/style/scss`
         },
         'zv-ui'
       ]
